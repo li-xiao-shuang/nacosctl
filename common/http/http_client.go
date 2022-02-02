d703878d834ca7c1d183f184276be34de7410f0c
lixiaoshuang <644968328@qq.com>
2022-02-02 15:36:50 +0800

    update log

--- common/http/http_client.go
+++ common/http/http_client.go
@@ -3,20 +3,26 @@ package http
 import (
 	"io/ioutil"
 	"nacosctl/common/logger"
+	"nacosctl/printer"
 	"net/http"
+	"os"
 )
 
 func Get(url string) string {
 	resp, err := http.Get(url)
 	if err != nil {
-		logger.Logger{}.Info("get request error,error:%s", err)
+		printer.Yellow(err)
 		return ""
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		logger.Logger{}.Info("reads an error,error:%s ", err)
+		printer.Yellow(err)
 		return ""
+	}
+	if resp.StatusCode == 403 {
+		printer.Red("[ERROR] Please log in your account first")
+		os.Exit(0)
 	}
 	if resp.StatusCode == 200 {
 		return string(body)
@@ -33,8 +39,12 @@ func Post(url string, param map[string][]string) string {
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		logger.Logger{}.Info("reads an error,error:%s ", err)
+		printer.Yellow(err)
 		return ""
+	}
+	if resp.StatusCode == 403 {
+		printer.Red("[ERROR] Please log in your account first")
+		os.Exit(0)
 	}
 	if resp.StatusCode == 200 {
 		return string(body)
@@ -46,14 +56,18 @@ func Delete(url string) string {
 	req, _ := http.NewRequest("DELETE", url, nil)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		logger.Logger{}.Info("delete request error,error:%s", err)
+		printer.Yellow(err)
 		return ""
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		logger.Logger{}.Info("reads an error,error:%s ", err)
+		printer.Yellow(err)
 		return ""
+	}
+	if resp.StatusCode == 403 {
+		printer.Red("[ERROR] Please log in your account first")
+		os.Exit(0)
 	}
 	if resp.StatusCode == 200 {
 		return string(body)
@@ -71,8 +85,12 @@ func Put(url string) string {
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		logger.Logger{}.Info("reads an error,error:%s ", err)
+		printer.Yellow(err)
 		return ""
+	}
+	if resp.StatusCode == 403 {
+		printer.Red("[ERROR] Please log in your account first")
+		os.Exit(0)
 	}
 	if resp.StatusCode == 200 {
 		return string(body)
